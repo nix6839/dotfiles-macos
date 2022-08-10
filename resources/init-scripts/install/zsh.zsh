d9945bb9ef91f2bf3b54d923adc933461f30928f
Han Yeong-woo <han@yeongwoo.dev>
2022-08-11 01:08:33 +0900

    Update environment variables

--- resources/init-scripts/install/zsh.zsh
+++ resources/init-scripts/install/zsh.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env /bin/zsh
 
-. "$HOME/.zshenv"
-
 git clone 'https://github.com/zplug/zplug' "$ZPLUG_HOME"
--- resources/init.zsh
+++ resources/init.zsh
@@ -3,4 +3,7 @@
 export CHEZMOI_RESOURCES_PATH="$(dirname "$0")"
 INIT_SCRIPT_PATH="$CHEZMOI_RESOURCES_PATH/init-scripts"
 
+. "$HOME/.zshenv"
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}"
+
 "$INIT_SCRIPT_PATH/install.zsh"
