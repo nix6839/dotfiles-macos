cc249815f3688836a378902d18a0012406fa6925
Han Yeong-woo <han@yeongwoo.dev>
2022-08-11 00:57:06 +0900

    Fix typo

--- resources/init.zsh
+++ resources/init.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env /bin/zsh
 
-export CHEZMOI_RESOURCES_PATH="$(dirname "$0")/resources"
+export CHEZMOI_RESOURCES_PATH="$(dirname "$0")"
 INIT_SCRIPT_PATH="$CHEZMOI_RESOURCES_PATH/init-scripts"
 
-"$CHEZMOI_INIT_SCRIPTS_PATH/install.zsh"
+"$INIT_SCRIPT_PATH/install.zsh"
