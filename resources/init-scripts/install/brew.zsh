36a13fa6607f4a6de618f2ebfba71848aeb2bad8
Han Yeong-woo <han@yeongwoo.dev>
2022-08-12 20:05:29 +0900

    Add shellcheck

--- resources/init-scripts/install/brew.zsh
+++ resources/init-scripts/install/brew.zsh
@@ -16,7 +16,7 @@ FONT_PACKAGES=(
 )
 CLI_PACKAGES=(
     'git' 'neovim' 'chezmoi' 'lsd' 'bat' 'tldr' 'git-delta' 'gnupg'
-    'pinentry-mac'
+    'pinentry-mac' 'shellcheck'
 )
 
 brew install $FONT_PACKAGES $CLI_PACKAGES
