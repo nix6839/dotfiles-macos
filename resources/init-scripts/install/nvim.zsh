37d25bec321fa9c821ed4c00b43a734455d4d1cb
Han Yeong-woo <han@yeongwoo.dev>
2022-08-17 08:10:13 +0900

    Add plug.vim

--- resources/init-scripts/install/nvim.zsh
+++ resources/init-scripts/install/nvim.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env /bin/zsh
 
-curl -fLo "$XDG_DATA_HOME/nvim/site/autoload/plug.vim" --create-dirs \
-    'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
-    nvim '+PlugInstall' '+qall'
+nvim '+PlugInstall' '+qall'
