c8763de8c0e3f0a9be625be7fccabde69f1bddda
Han Yeong-woo <han@yeongwoo.dev>
2022-08-29 21:58:26 +0900

    Switch `update-all` to function

--- private_Library/private_Application Support/zsh/aliases.zsh
+++ private_Library/private_Application Support/zsh/aliases.zsh
@@ -17,8 +17,3 @@ alias lR='ls -R'
 alias llR='ls -lR'
 alias laR='ls -AR'
 alias llaR='ls -lAR'
-
-alias update-all='brew update && brew upgrade && mas upgrade && zplug update &&
-  tldr --update && nvim +PlugUpgrade +PlugInstall +qall &&
-  ncu --packageFile $PNPM_HOME/global/5/package.json -u && pnpm i -g &&
-  ncu -g'
