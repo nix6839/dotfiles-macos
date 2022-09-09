7ee232d7116dbd3239a5128a1328eab477af4464
Han Yeong-woo <nix6839@washnix.com>
2022-09-09 19:23:56 +0900

    script(python): install autopep8 and pylint

--- resources/init-scripts/install/python.zsh
+++ resources/init-scripts/install/python.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env /bin/zsh
 
-curl -sSL https://install.python-poetry.org | python3 -
+pip3 install --upgrade pip && pip3 install autopep8 pylint &&
+  curl -sSL https://install.python-poetry.org | python3 -
