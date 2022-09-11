e02fda306d4325dc669f1e7cfeb1780ae2abb6e1
Han Yeong-woo <nix6839@washnix.com>
2022-09-12 01:42:06 +0900

    node: add package create-svelte

--- resources/init-scripts/install/node.zsh
+++ resources/init-scripts/install/node.zsh
@@ -4,7 +4,7 @@ NVM_SCRIPT_URL='https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh'
 
 declare -a NODE_PACKAGES
 NODE_PACKAGES=(
-    'create-next-app' 'create-vite' 'create-turbo' '@nestjs/cli'
+    'create-next-app' 'create-vite' 'create-svelte' 'create-turbo' '@nestjs/cli'
     'typescript' 'ts-node' 'nodemon' 'localtunnel' 'npm-check-updates'
     'sort-package-json'
 )
