46ccead6adf284aa83fa9d55772c10c9678a64bd
Han Yeong-woo <nix6839@washnix.com>
2022-10-01 10:13:59 +0900

    script(brew): now support m1 native remix ide

--- resources/init-scripts/install/brew.zsh
+++ resources/init-scripts/install/brew.zsh
@@ -26,9 +26,9 @@ declare -a CASK_PACKAGES
 CASK_PACKAGES=(
     'gureumkim' 'microsoft-edge' 'alacritty' 'discord' 'bitwarden'
     'visual-studio-code' 'notion' 'slack' 'spotify' 'telegram-desktop'
-    'dropbox'
+    'dropbox' 'remix-ide'
     # Does not support m1 native
-    'remix-ide' 'tutanota'
+    'tutanota'
 )
 
 brew install --cask $CASK_PACKAGES
