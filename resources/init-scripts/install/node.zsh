933c3cad07f6a29e1858d97459ffaa56b7a31715
Han Yeong-woo <nix6839@washnix.com>
2022-12-28 06:59:20 +0900

    node: remove localtunnel

--- resources/init-scripts/install/node.zsh
+++ resources/init-scripts/install/node.zsh
@@ -5,8 +5,7 @@ NVM_SCRIPT_URL='https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh'
 declare -a NODE_PACKAGES
 NODE_PACKAGES=(
     'create-next-app' 'create-vite' 'create-svelte' 'create-turbo' '@nestjs/cli'
-    'typescript' 'ts-node' 'nodemon' 'localtunnel' 'npm-check-updates'
-    'sort-package-json'
+    'typescript' 'ts-node' 'nodemon' 'npm-check-updates' 'sort-package-json'
 )
 
 curl -o- "$NVM_SCRIPT_URL" | bash && . "$NVM_DIR/nvm.sh" &&
