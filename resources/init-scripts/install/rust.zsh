dfce95593585f907d372d55071f097c8ba1bd0d2
Han Yeong-woo <han@yeongwoo.dev>
2023-11-01 18:11:33 +0900

    rust: add packages

--- resources/init-scripts/install/rust.zsh
+++ resources/init-scripts/install/rust.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env /bin/zsh
 
+declare -a RUST_PACKAGES
+RUST_PACKAGES=(
+  'cargo-update' 'cargo-license'
+)
+
 curl --proto '=https' --tlsv1.2 -sSf 'https://sh.rustup.rs' |
-  sh -s -- -y --no-modify-path
+  sh -s -- -y --no-modify-path && cargo install $RUST_PACKAGES
