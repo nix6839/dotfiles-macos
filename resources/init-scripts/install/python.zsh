c356dba1caa56464d043d53395a6700a6e6d859f
Han Yeong-woo <han@yeongwoo.dev>
2023-11-14 08:59:39 +0900

    python: install python 3.12

--- resources/init-scripts/install/python.zsh
+++ resources/init-scripts/install/python.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env /bin/zsh
 
-pip3.10 install --upgrade pip && pip3.10 install autopep8 pylint &&
-  curl -sSL https://install.python-poetry.org | python3.10 -
+pip3.12 install --upgrade pip && pip3.12 install autopep8 pylint &&
+  curl -sSL https://install.python-poetry.org | python3.12 -
